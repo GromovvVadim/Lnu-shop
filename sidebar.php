7249ff4563d63f0ccd44aa1060de1149db5c87a6
DanyloTymets <56023366+DanyloTymets@users.noreply.github.com>
2020-05-14 22:52:27 +0300

    Added basic shop page and sidebar

---
+++ sidebar.php
@@ -0,0 +1,60 @@
+
+
+<div class="panel panel-default sidebar-menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            Product Categories
+            <div class="pull-right">
+                <a href="JavaScript:Void(0);" style="color:black;">
+                    <span class="nav-toggle hide-show">
+                        Hide
+                    </span>
+                </a>
+            </div>
+        </h3>
+    </div>
+    <div class="panel-collapse collapse-data">
+        <div class="panel-body"> 
+            <div class="input-group"> 
+                <input type="text" class="form-control" id="dev-table-filter" data-filters="#dev-p-cat" data-action="filter" placeholder="Filter Product Categories">
+                    <a class="input-group-addon">
+                        <i class="fa fa-search"></i>
+                    </a>
+            </div>
+            </div> 
+        <div class="panel-body scroll-menu"> 
+            <ul class="nav nav-pills nav-stacked category-menu" id="dev-p-cat">
+               
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="panel panel-default sidebar-menu"> 
+    <div class="panel-heading"> 
+        <h3 class="panel-title">
+            Customer Categories
+            <div class="pull-right">
+                <a href="JavaScript:Void(0);" style="color:black;">
+                    <span class="nav-toggle hide-show">
+                        Hide
+                    </span>
+                </a>
+            </div>
+        </h3>
+    </div>
+    <div class="panel-collapse collapse-data">
+        <div class="panel-body">
+            <div class="input-group">
+                <input type="text" class="form-control" id="dev-table-filter" data-filters="#dev-cat" data-action="filter" placeholder="Filter Categories">
+                    <a class="input-group-addon">
+                        <i class="fa fa-search"></i>
+                    </a>
+            </div>
+            </div>
+        <div class="panel-body scroll-menu">
+            <ul class="nav nav-pills nav-stacked category-menu" id="dev-cat"> 
+                
+            </ul>
+        </div>
+    </div>
+</div>
