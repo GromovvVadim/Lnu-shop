8071b5f34a173aa7b0caca760f4ca90957ab3c72
DanyloTymets <56023366+DanyloTymets@users.noreply.github.com>
2020-05-15 13:48:03 +0300

    Added details page and new styles

---
+++ details.php
@@ -0,0 +1,249 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TD Shop</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body> 
+   <div id="top">
+       <div class="container">
+           <div class="col-md-6 offer">
+            <a href="#" class="btn btn-success btn-primary">
+                   
+               </a>
+               <a href="checkout.php" style="color:white"> </a>
+           </div>
+           <div class="col-md-6">
+               
+               <ul class="menu">
+                   
+                   <li>
+                       <a href="cart.php">Check Cart</a>
+                   </li>
+                   <li>
+                       <a href="checkout.php">Account</a>
+                   </li>
+                   <li>
+                    <a href="checkout.php">
+                     
+                     </a>
+                   </li>
+                   <li>
+                       <a href="register.php">Register</a>
+                   </li> 
+               </ul>
+           </div>
+       </div>
+   </div>
+   <div id="navbar" class="navbar navbar-default">
+       <div class="container">
+           <div class="navbar-header">
+               <a href="index.php" class="navbar-brand home">
+                   <img src="images/Webshop-logo.png" alt="Webshop Logo" class="hidden-xs">
+                   <img src="images/Webshop-logo-mobile.png" alt="Webshop Logo Mobile" class="visible-xs"> 
+               </a>
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                   
+                   <span class="sr-only">Toggle Navigation</span>
+                   
+                   <i class="fa fa-align-justify"></i> 
+               </button> 
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#search"> 
+                   <span class="sr-only">Toggle Search</span> 
+                   <i class="fa fa-search"></i> 
+               </button> 
+           </div>
+           
+           <div class="navbar-collapse collapse" id="navigation">
+               <div class="padding-nav">
+                   <ul class="nav navbar-nav left">
+                       <li class="active">
+                           <a href="index.php">Home</a>
+                       </li>
+                       <li>
+                           <a href="shop.php">Shop</a>
+                       </li>
+                       <li>
+                           <a href="cart.php">Shopping Cart</a>
+                       </li>
+                       <li>
+                           <a href="contact.php">Contacts</a>
+                       </li>
+                        <li>
+                          
+                       </li> 
+                   </ul>
+               </div>
+               <a href="cart.php" class="btn navbar-btn btn-primary right">
+                   <i class="fa fa-shopping-cart"></i> 
+                   <span></span> 
+               </a> 
+               <div class="navbar-collapse collapse right">
+                   <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search">
+                       <span class="sr-only">Toggle Search</span> 
+                       <i class="fa fa-search"></i> 
+                   </button>
+               </div>
+               
+               <div class="collapse clearfix" id="search">
+                   <form method="get" action="results.php" class="navbar-form">
+                       <div class="input-group">
+                           <input type="text" class="form-control" placeholder="Search" name="user_query" required> 
+                           <span class="input-group-btn"> 
+                           <button type="submit" name="search" value="Search" class="btn btn-primary">
+                               <i class="fa fa-search"></i> 
+                           </button>
+                           </span> 
+                       </div>
+                   </form>   
+               </div>
+           </div>
+       </div> 
+   </div>
+   
+   <div id="content">
+       <div class="container">
+           <div class="col-md-12">
+               <!--<ul class="breadcrumb">
+                   <li>
+                       <a href="index.php">Home</a>
+                   </li>
+                   <li>
+                       Shop
+                   </li>
+               </ul>  -->
+               
+           </div>
+           <div class="col-md-3">
+     <?php  
+      include("sidebar.php");
+      ?>
+           </div>
+           <div class="col-md-9">
+               <div id="productMain" class="row">
+                   <div class="col-sm-6">
+                       <div id="mainImage">
+                           <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                               <ol class="carousel-indicators">
+                                   <li data-target="#myCarousel" data-slide-to="0" class="active" ></li>
+                                   <li data-target="#myCarousel" data-slide-to="1"></li>
+                                   <li data-target="#myCarousel" data-slide-to="2"></li>
+                               </ol>
+                               <div class="carousel-inner">
+                                   <div class="item active">
+                                       <center><img class="img-responsive" src="admin_area/product_images/<?php echo $pro_img1; ?>" alt="Product 1"></center>
+                                   </div>
+                                   <div class="item">
+                                       <center><img class="img-responsive" src="admin_area/product_images/<?php echo $pro_img1; ?>" alt="Product 1"></center>
+                                   </div>
+                                   <div class="item">
+                                       <center><img class="img-responsive" src="admin_area/product_images/<?php echo $pro_img1; ?>" alt="Product 1"></center>
+                                   </div>
+                               </div>
+                               
+                               <a href="#myCarousel" class="left carousel-control" data-slide="prev">
+                                   <span class="glyphicon glyphicon-chevron-left"></span>
+                                   <span class="sr-only">Previous</span>
+                               </a>
+                               
+                               <a href="#myCarousel" class="right carousel-control" data-slide="next">
+                                   <span class="glyphicon glyphicon-chevron-right"></span>
+                                   <span class="sr-only">Next</span>
+                               </a>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-sm-6">
+                       <div class="box">
+                           <h1 class="text-center"></h1>
+                           
+                           <form action="details.php?add_cart=<?php echo $product_id; ?>" class="form-horizontal" method="post">
+                               <div class="form-group">
+                                   <label for="" class="col-md-5 control-label">Products Quantity</label>
+                                   <div class="col-md-7">
+                                          <select name="product_qty" id="" class="form-control">
+                                           <option>1</option>
+                                           <option>2</option>
+                                           <option>3</option>
+                                           <option>4</option>
+                                           <option>5</option>
+                                           </select>
+                                    </div>
+                               </div>
+                               <p class="price">$ </p>
+                               <p class="text-center buttons"><button class="btn btn-primary i fa fa-shopping-cart"> Add to cart</button></p>
+                           </form>
+                       </div>
+                       
+                       <!--<div class="row" id="thumbs">
+                           <div class="col-xs-4">
+                               <a href="#" class="thumb">
+                                   <img src="admin_area/product_images/product-1.jpg" alt="product 1" class="img-responsive">
+                               </a>
+                           </div>
+                           
+                           <div class="col-xs-4">
+                               <a href="#" class="thumb">
+                                   <img src="admin_area/product_images/product-2.jpg" alt="product 2" class="img-responsive">
+                               </a>
+                           </div>
+                           
+                           <div class="col-xs-4">
+                               <a href="#" class="thumb">
+                                   <img src="admin_area/product_images/Product-4a.jpg" alt="product 4" class="img-responsive">
+                               </a>
+                           </div>
+                           
+                       </div>-->
+                       
+                   </div>
+               </div>
+               
+               <div class="box info1" id="details">
+                  <h4>Product Details</h4>
+                  <p>
+                    
+                  </p> 
+               </div>
+               
+               <div id="row same-heigh-row">
+                   <div class="col-md-3 col-sm-6">
+                       <div class="box same-height headline">
+                           <h3 class="text-center">Also You May be Interested In</h3>
+                       </div>
+                   </div>
+                   
+               </div>
+           </div>
+           <!--<div id="disqus_thread"></div>
+            <script>
+            (function() {
+            var d = document, s = d.createElement('script');
+            s.src = 'https://http-localhost-horondiecommerceweb.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+            })();
+            </script>-->
+            <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v5.0&appId=1273845056156438&autoLogAppEvents=1"></script>
+            <div class="fb-comments" data-href="http://localhost/webshop-main/details.php" data-width="" data-numposts="5"></div>
+       </div>
+   </div>
+   
+   <?php 
+    
+    include("footer.php");
+    
+    ?>
+    
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    
+    
+</body>
+</html>
