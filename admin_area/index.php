875d2842f3ddee8f295edd3d1533cc7c8dd33704
DanyloTymets <56023366+DanyloTymets@users.noreply.github.com>
2020-05-17 11:17:43 +0300

    Created admin folder and added insert_product page

---
+++ admin_area/index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LNU Shop Admin Page</title>
+    <link rel="stylesheet" href="css/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head> 
+    <div id="wrapper"> 
+       <?php include("includes/sidebar.php"); ?>
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                
+            </div>
+        </div>
+    </div>
+<script src="js/jquery-331.min.js"></script>     
+<script src="js/bootstrap-337.min.js"></script>           
+</body>
+</html>
