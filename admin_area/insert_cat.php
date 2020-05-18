69795e399e528e11664a1e2e15c18861beb84365
DanyloTymets <56023366+DanyloTymets@users.noreply.github.com>
2020-05-18 11:30:00 +0300

    Added insert pages to admin area

---
+++ admin_area/insert_cat.php
@@ -0,0 +1,50 @@
+
+<div class="row"> 
+    <div class="col-lg-12"> 
+        <!--<ol class="breadcrumb"> 
+            <li> 
+                <i class="fa fa-dashboard"></i> Dashboard / Insert Category  
+            </li>
+        </ol>--> 
+    </div> 
+</div> 
+<div class="row"> 
+    <div class="col-lg-12"> 
+        <div class="panel panel-default"> 
+            <div class="panel-heading"> 
+                <h3 class="panel-title"> 
+                    <i class="fa fa-money fa-fw"></i> Insert Customer Category 
+                </h3> 
+            </div> 
+            <div class="panel-body"> 
+                <form action="" class="form-horizontal" method="post"> 
+                    <div class="form-group"> 
+                        <label for="" class="control-label col-md-3"> 
+                           Customer Category Title  
+                        </label> 
+                        <div class="col-md-6"> 
+                            <input name="cat_title" type="text" class="form-control"> 
+                        </div> 
+                    </div> 
+                    <div class="form-group"> 
+                        <label for="" class="control-label col-md-3"> 
+                           Customer Category Description 
+                        </label> 
+                        <div class="col-md-6"> 
+                            <textarea type='text' name="cat_desc" id="" cols="30" rows="10" class="form-control"></textarea> 
+                        </div> 
+                    </div> 
+                    <div class="form-group"> 
+                        <label for="" class="control-label col-md-3"> 
+                        </label> 
+                        <div class="col-md-6"> 
+                            <input value="Add Category" name="submit" type="submit" class="form-control btn btn-primary"> 
+                        </div> 
+                    </div> 
+                </form> 
+            </div> 
+        </div> 
+    </div> 
+</div> 
+ 
+<?php } ?>
---
+++ admin_area/insert_p_cat.php
@@ -0,0 +1,52 @@
+
+<div class="row"> 
+    <div class="col-lg-12"> 
+        <!--<ol class="breadcrumb"> 
+            <li> 
+                <i class="fa fa-dashboard"></i> Dashboard / Insert Product Category 
+            </li>
+        </ol>--> 
+    </div> 
+</div>
+<div class="row"> 
+    <div class="col-lg-12"> 
+        <div class="panel panel-default"> 
+            <div class="panel-heading"> 
+                <h3 class="panel-title">
+                    <i class="fa fa-money fa-fw"></i> Insert Product Category 
+                </h3> 
+            </div>
+            <div class="panel-body"> 
+                <form action="" class="form-horizontal" method="post"> 
+                    <div class="form-group">
+                        <label for="" class="control-label col-md-3">
+                            Product Category Title
+                        </label>
+                        <div class="col-md-6">
+                            <input name="p_cat_title" type="text" class="form-control"> 
+                        </div>
+                    </div> 
+                    <div class="form-group">
+                        <label for="" class="control-label col-md-3">
+                            Product Category Description
+                        </label>
+                        <div class="col-md-6">
+                            <textarea type='text' name="p_cat_desc" id="" cols="30" rows="10" class="form-control"></textarea> 
+                        </div>
+                    </div> 
+                    <div class="form-group">
+                        <label for="" class="control-label col-md-3">
+                        </label>
+                        <div class="col-md-6">
+                            <input value="Add Category" name="submit" type="submit" class="form-control btn btn-primary"> 
+                        </div>
+                    </div> 
+                </form> 
+            </div> 
+            
+        </div> 
+    </div> 
+</div> 
+
+ 
+<?php } ?> 
