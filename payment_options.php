afde1791963d62dcfe9c0ee71d7d60c58eda6abf
DanyloTymets <56023366+DanyloTymets@users.noreply.github.com>
2020-05-18 11:31:35 +0300

    Added payment_options page

---
+++ payment_options.php
@@ -0,0 +1,15 @@
+<div class="box"> 
+
+    <h1 class="text-center">Payment Options</h1>
+     <p class="lead text-center">
+        <a href="order.php?c_id=<?php echo $customer_id ?>"> Offline Payment </a>
+     </p>
+     <center>
+        <p class="lead">
+            <a href="#"> 
+                Paypall Payment 
+                <img class="img-responsive" src="images/Paypal-logo.jpg" alt="img-paypall"> 
+            </a> 
+        </p>
+     </center>
+</div>
